07c6b2b7f842f854428b2981c848ddef74cfe06c
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-18 23:29:55 +0300

    thirdsec hover

--- src/components/ThirdSec.jsx
+++ src/components/ThirdSec.jsx
@@ -4,14 +4,14 @@ const ThirdSec = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex gap-4 flex-col items-center w-2/4 text-center mx-auto">
-        <div className="lg:text-[55px] font-poppins font-medium md:text-[30px] text-[21px] text-[#2092CF]">
+        <div className="lg:text-[55px] font-poppins hover:text-[59px] transition-all font-medium md:text-[30px] text-[21px] text-[#2092CF]">
           Our platform makes scheduling simple
         </div>
-        <div className="lg:text-[19px] font-poppins md:text-[14px] text-[9px]">
+        <div className="lg:text-[19px] hover:text-[21px] transition-all font-poppins md:text-[14px] text-[9px]">
           Our platform was designed to make scheduling simple and effortless. With its intuitive interface and streamlined workflows, the process of coordinating appointments becomes seamless and stress-free.
         </div>
         <div>
-          <button className="bg-[#2092CF] font-poppins text-white p-2 lg:px-14 px-2 text-sm rounded-sm">
+          <button className="bg-[#2092CF] hover:bg-white hover:ring-2 hover:ring-[#2092CF] hover:text-[#2092CF] transition-all font-poppins text-white p-2 lg:px-14 px-2 text-sm rounded-[4px]">
             Register
           </button>
         </div>
