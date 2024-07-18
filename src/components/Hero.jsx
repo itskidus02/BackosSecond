b197efa98e740033fc2c64da2ebf196ad9b1d5c7
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-18 19:35:23 +0300

    only font and circle left

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,17 +5,17 @@ const Hero = () => {
   return (
     <div className="flex items-center flex-col lg:flex-row justify-center w-3/4 mx-auto space-x-20 mt-[100px] rig-2 ing-blue-300 bg-white p-6 relative">
       <div className="text-center md:text-left ing-2 rng-red-300 lg:w-2/2">
-        <h1 className="text-6xl font-bold leading-tight">
-          Effortless <span className="text-blue-500"> <br /> Scheduling</span>, 
+        <h1 className="lg:text-6xl text-3xl font-bold lg:leading-tight">
+          Effortless <span className="text-[#2092CF]"> <br /> Scheduling</span>, 
           Automated for <br /> Your Convenience
         </h1>
-        <button className="mt-6 px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg">
+        <button className="mt-6 px-10 rounded-[2px] py-1 bg-[#2092CF] text-white shadow-lg">
           Get Started
         </button>
       </div>
-      <div className="w-full lg:w-1/2 flex justify-center rig-2 rig-green-300 relative mt-20 lg:mt-0">
-        <div className="absolute -top-10 left-0 w-[350px] h-[380px] rounded-3xl bg-blue-100  transform  z-10"></div>
-        <img src={calander} alt="Calendar" className="relative rounded-3xl z-20 w-[350px] h-[380px]" />
+      <div className="w-full lg:w-1/2 flex justify-center relative mt-20 lg:mt-0">
+        <div className="absolute md:-top-8 md:left-11 lg:-top-10 lg:left-0 lg:w-[350px] lg:h-[380px] w-[250px] h-[280px] -top-10 -left-10 rounded-3xl bg-blue-100  transform  z-10"></div>
+        <img src={calander} alt="Calendar" className="relative md:right-12 lg:left-2 rounded-3xl z-20 lg:w-[350px] lg:h-[380px] w-[250px] h-[280px]" />
       </div>
     </div>
   );
