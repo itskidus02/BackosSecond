c05c6266344788ccd51ba3507d2920b6e8682f25
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-18 21:37:24 +0300

    how done

--- src/components/How.jsx
+++ src/components/How.jsx
@@ -10,7 +10,7 @@ const How = () => {
       
       <div className="relative z-10 py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 mx-auto lg:mb-16 text-center">
-          <h2 className="mb-4 text-4xl tracking-tight mx-auto font-extrabold text-[#2092CF]">
+          <h2 className="mb-4 font-poppins text-4xl tracking-tight mx-auto font-medium text-[#2092CF]">
             How it works
           </h2>
         </div>
@@ -21,8 +21,8 @@ const How = () => {
                 1
               </p>
             </div>
-            <h3 className="mb-6 text-3xl lg:text-3xl font-bold text-[#5A5555] ">Register Subscription</h3>
-            <p className="text-[#5A5555] text-2xl font-light lg:text-3xl">
+            <h3 className="mb-6 text-3xl lg:text-3xl font-bold font-poppins text-[#5A5555] ">Register Subscription</h3>
+            <p className="text-[#5A5555] text-2xl font-poppins lg:text-2xl font-light ">
               Sign up your company by providing the necessary information. After the verification process, you will receive admin credentials to access the dashboard.
             </p>
           </div>
@@ -32,8 +32,8 @@ const How = () => {
                 2
               </p>
             </div>
-            <h3 className="mb-6 text-3xl lg:text-3xl font-bold text-[#5A5555] ">Set Up Admin Dashboard</h3>
-            <p className="text-[#5A5555] text-2xl font-light lg:text-3xl">
+            <h3 className="mb-6 text-3xl lg:text-3xl font-bold text-[#5A5555] font-poppins ">Set Up Admin Dashboard</h3>
+            <p className="text-[#5A5555] text-2xl font-light font-poppins lg:text-2xl">
               Log in to the admin dashboard using the provided credentials to manage your company's settings and configurations.
             </p>
           </div>
@@ -43,8 +43,8 @@ const How = () => {
                 3
               </p>
             </div>
-            <h3 className="mb-6 text-3xl lg:text-3xl font-bold text-[#5A5555] ">Add Employees</h3>
-            <p className="text-[#5A5555] text-2xl font-light lg:text-3xl">
+            <h3 className="mb-6 text-3xl lg:text-3xl font-bold text-[#5A5555] font-poppins">Add Employees</h3>
+            <p className="text-[#5A5555] text-2xl font-light font-poppins lg:text-2xl">
               Onboard your employees and have them add their details, including their name, roles, and available time slots.
             </p>
           </div>
@@ -54,8 +54,8 @@ const How = () => {
                 4
               </p>
             </div>
-            <h3 className="mb-6 text-3xl lg:text-3xl font-bold text-[#5A5555] ">Manage Bookings</h3>
-            <p className="text-[#5A5555] text-2xl font-light lg:text-3xl">
+            <h3 className="mb-6 text-3xl lg:text-3xl font-bold text-[#5A5555] font-poppins ">Manage Bookings</h3>
+            <p className="text-[#5A5555] text-2xl font-light font-poppins lg:text-2xl">
               Your clients can now easily book appointments with the right employee through the platform's intuitive booking system.
             </p>
           </div>
