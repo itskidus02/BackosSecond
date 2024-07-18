3d793dfc929a1ffde6663a02aa9576f7de814f36
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-18 20:41:45 +0300

    only one circle left

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import  alarm  from "../assets/alarm.png";
-import  tick  from "../assets/tick.png";
-import bag  from "../assets/bag.png";
+import alarm from "../assets/alarm.png";
+import tick from "../assets/tick.png";
+import bag from "../assets/bag.png";
 
 const Service = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen ">
+    <div className="flex items-center justify-center min-h-screen relative">
+      {/* Blue circle overlaying the outside div */}
+      <div className="hidden lg:block absolute bottom-[14rem] left-36 transform z-10 overflow-hidden">
+        <div className="w-28 h-28 bg-[#BCE1F5] rounded-full"></div>
+      </div>
+      
       <div className="relative py-16 overflow-hidden w-3/4 mx-auto">
         <div className="absolute rounded-[30px] inset-0 bg-[#2092CF] h-64"></div>
+        
         <div className="relative z-10 container mx-auto px-4">
           <div className="flex lg:flex-row flex-col justify-between items-center mb-8">
             <h2 className="lg:text-3xl text-[20px] font-bold ml-5 text-white">
@@ -22,9 +28,7 @@ const Service = () => {
           <div className="relative text-center z-20 grid grid-cols-1 md:grid-cols-3 mx-auto gap-4">
             <div className="relative rounded-[15px] p-12 m-4 bg-[#FAFAFA] text-black shadow-md transform translate-y-[-20px] h-80">
               <div className="flex items-center justify-center text-4xl text-center mb-2">
-                {" "}
-                <img src={alarm}
-                className="w-[55px] h-[50px] text-center" />
+                <img src={alarm} className="w-[55px] h-[50px] text-center" />
               </div>
               <h3 className="text-xl font-semibold text-[#5A5555] mb-6 mt-7">
                 Easy to use
@@ -34,24 +38,20 @@ const Service = () => {
               </p>
             </div>
             <div className="relative rounded-[15px] bg-[#FAFAFA] text-black p-12 m-4 shadow-md transform translate-y-[-20px] h-80">
-              <div className=" flex items-center justify-center text-4xl mb-2">
-                <img src={bag}
-                className="w-[55px] h-[50px] text-center" />
+              <div className="flex items-center justify-center text-4xl mb-2">
+                <img src={bag} className="w-[55px] h-[50px] text-center" />
               </div>
               <h3 className="text-xl font-semibold mb-6 mt-7 text-[#5A5555]">
                 Custom Booking page
               </h3>
               <p className="lg:text-[16px] text-[10px] text-[#5A5555]">
-                {" "}
                 Enhance your professional business with a customized booking
                 page.
               </p>
             </div>
             <div className="relative rounded-[15px] bg-[#FAFAFA] text-black p-12 m-4 shadow-md transform translate-y-[-20px] h-80">
-              <div className=" flex items-center justify-center text-4xl mb-2 ">
-                {" "}
-                <img src={tick}
-                className="w-[55px] h-[50px] text-center" />
+              <div className="flex items-center justify-center text-4xl mb-2">
+                <img src={tick} className="w-[55px] h-[50px] text-center" />
               </div>
               <h3 className="text-xl font-semibold mb-6 mt-7 text-[#5A5555]">
                 Round the clock support
