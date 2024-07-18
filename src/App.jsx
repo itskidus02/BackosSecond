f94baf3b4110041cdbf4d82ddea03f432f0f262d
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-18 16:33:52 +0300

    only circle left for how

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Service from './components/Service'
  import Header from './components/Header'
 //  import Hero from './components/Hero'
 // import ThirdSec from './components/ThirdSec'
-// import How from './components/How'
-//  import Accordion from './components/Accordion'
+import How from './components/How'
 // import Bottom from './components/Bottom'
 // import { AccordionDemo } from './components/Demo/AccordionDemo'
 
@@ -17,16 +16,16 @@ function App() {
   return (
     <>
      <Header/>
-    {/* } <Hero/>   
-    <ThirdSec/>
+    {/* <Hero/>    */}
+    {/* <ThirdSec/> */}
     <How/>
     
-   <AccordionDemo/>
+   {/* <AccordionDemo/> */}
 
     
-    <Bottom/> 
-     */}
-    <Service/>  
+    {/* <Service/>   */}
+    {/* <Bottom/>  */}
+    
     </>
   )
 }
