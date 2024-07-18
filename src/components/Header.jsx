3fa7de4cc9363e66fad4c5e0137513ff35fbdea6
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-18 21:50:58 +0300

    everything done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,18 @@ import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div className="flex md:flex-row justify-around shadow-md items-center p-1">
+    <div className="flex flex-col md:flex-row justify-around shadow-md items-center p-1">
       <div className="mt-1 w-[55px] h-[50px] md:mb-0">
         <img src={logo} alt="Logo" />
       </div>
-      <div className=" md:flex space-x-4 font-poppins md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+      <div className="flex flex-col md:flex-row md:space-x-4 font-poppins space-y-2 md:space-y-0 md:space-x-4">
         <button className="text-sm hover:text-[#2092CF]">Home</button>
         <button className="text-sm hover:text-[#2092CF]">Contact Us</button>
         <button className="text-sm hover:text-[#2092CF]">Features</button>
         <button className="text-sm hover:text-[#2092CF]">Pricing</button>
       </div>
       <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 mt-2 md:mt-0">
-        <button className="text-[#2092CF] font-roboto hover:text-black  text-sm p-2">Log in</button>
+        <button className="text-[#2092CF] font-roboto hover:text-black text-sm p-2">Log in</button>
         <button className="bg-[#2092CF] lg:px-4 px-2 py-2 rounded-[4px] hover:bg-white hover:ring-2 hover:ring-[#2092CF] hover:text-[#2092CF] text-white text-xs">
           Get Started
         </button>
